a0894d3619688f84594e220b6d4d3b70c7a0bf9c
ScorpiusSigma <justinleng_@hotmail.com>
2023-03-01 19:47:15 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -7,8 +7,8 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.RemarkCommand;
-import seedu.address.model.person.Remark;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Remark;
 
 /**
  * Parses input arguments and creates a new {@code RemarkCommand} object
